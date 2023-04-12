681cb5335362f11abc640c67986eb4df11168f20
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-12 08:41:07 +0700

    cakra

---
+++ src/main/java/com/project/payment/model/list_transaksi.java
@@ -0,0 +1,28 @@
+package com.project.payment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class list_transaksi {
+	
+	@Id
+	@Column(name="id_transaksi")
+	private int id_transaksi;
+	@Column(name = "nama_paket")
+	private String nama_paket;
+	@Column(name="nama_pelanggan")
+	private String nama_pelanggan;
+	@Column(name="nominal")
+	private Double nominal;
+	@Column(name="nomor_rek")
+	private int nomor_rek;
+	@Column(name="status_transaksi")
+	private String status_transaksi;
+	
+}
