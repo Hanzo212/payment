6fbca45e9577de84f877a2d158d7744003a3848f
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-14 14:42:44 +0700

    cakra

--- src/main/java/com/project/payment/controller/ListTransaksiController.java
+++ src/main/java/com/project/payment/controller/ListTransaksiController.java
@@ -1,5 +1,9 @@
 package com.project.payment.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ListTransaksiController {
 
     //insert transaksi
@@ -7,4 +11,9 @@ public class ListTransaksiController {
     //kirim invoice
 
     //inquiry all transaksi by id pelanggan
+	@GetMapping("/txnBy")
+	public List<ListTransaksiController> getTxnBy(){
+		return null;
+		
+	}
 }
